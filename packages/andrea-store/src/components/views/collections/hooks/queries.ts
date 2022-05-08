f835736a109b349e9f39ab5fbafa2a7ead238244
jongkeun-kim <kjkandrea@gmail.com>
2022-05-08 17:20:44 +0900

    Add TODO

--- packages/andrea-store/src/components/views/collections/hooks/queries.ts
+++ packages/andrea-store/src/components/views/collections/hooks/queries.ts
@@ -2,6 +2,13 @@ import { useQuery } from 'react-query';
 import { client } from '../../../../main';
 import { Collection } from '../types';
 
+/**
+ * TODO
+ * collection === 'ALL' 일때
+ * client.product.fetchAll 을 받아오는것은 비즈니스 규칙이다.
+ * 이를 어떻게 분리해야할까?
+ */
+
 export const useCollections = (collection: Collection) =>
   useQuery(['collections', collection], () => {
     return client.product.fetchAll();
